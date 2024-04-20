46353c1c631d444a3b733b18f3870eef836c5db1
rodri-4002 <166069557+rodri-4002@users.noreply.github.com>
2024-04-20 16:52:10 -0500

    Add files via upload

---
+++ CapaEntidad/Alumno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapaEntidad
+{
+    public class Alumno : Persona
+    {
+        private int codigo;
+        private int telefono;
+
+        public int Codigo
+        {
+            get { return this.codigo; }
+            set { this.codigo = value; }
+        }
+
+        public int Telefono
+        {
+            get { return this.telefono; }
+            set { this.telefono = value; }
+        }
+
+        public string DevolverDatosAlumno()
+        {
+            return "Codigo: " + this.Codigo + " Apellidos: " + this.Apellidos + " Nombres: " +
+            this.Nombre + " Edad: " + Convert.ToString(this.Edad) + " Telefono: " +
+            this.Telefono;
+        }
+    }
+
+}
---
+++ CapaEntidad/Persona.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Persona
+    {
+        
+        
+        //Conatructores 
+        public Persona() //Constructor sin parametros
+        { }
+
+        public Persona(string Nombres)
+        {
+            this.nombres = Nombres;
+        }
+
+        public Persona(string Nombres, string Apellidos)
+        {
+            this.nombres = Nombres;
+            this.apellidos = Apellidos;
+        }
+
+        public Persona(string Nombres, string Apellidos, int Edad)
+        {
+            this.nombres = Nombres;
+            this.apellidos = Apellidos;
+            this.edad = Edad;
+        }
+        //Declarar atributos
+        private string nombres;
+        private string apellidos;
+        private int edad;
+
+        //Declarar propiedades 
+        public string Nombre
+        {
+            get { return this.nombres; }
+            set { this.nombres = value; }
+
+        }
+        public string Apellidos
+        {
+            get { return this.apellidos; }
+            set { this.apellidos = value; }
+        }
+        public int Edad
+        {
+            get { return this.edad; }
+            set { this.edad = value; }
+        }
+
+        //Metodos
+
+        public string DevolverDatos()
+        {
+            return "Apellidos: " + this.apellidos + " Nombres:  " + this.nombres + " Edad: " +
+                Convert.ToString(this.edad);
+        }
+    }
+}
